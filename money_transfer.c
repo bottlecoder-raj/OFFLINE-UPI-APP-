0e9f238f90477e034fac30b7d8b12d9a6c0d101b
Raj Narayan <hazrarajnarayan55@gmail.com>
2025-01-10 00:26:29 +0530

    Update money_transfer.c
    added message commands to interpret and turned on for serial communication

--- money_transfer.c
+++ money_transfer.c
@@ -10,18 +10,23 @@ int main()
     float balance;
     float se_m;
     float re_m;
+    char send_mobile[13];
+    char rece_mobile[13];
     char line[256];
     int found = 0;
+    int round = 0;
     char *s_token;
     char *r_token;
 
     const char *filename = "data.txt";
     const char *tempfile = "temp.txt";
+    const char *messfile = "mess.txt";
 
     FILE *file = fopen("data.txt", "r");
     FILE *temp = fopen("temp.txt", "w");
-
-    if (file == NULL || temp == NULL)
+    FILE *mess = fopen("mess.txt", "w");
+   
+    if (file == NULL || temp == NULL || mess == NULL)
     {
         perror("Error opening file");
         exit(1);
@@ -43,6 +48,10 @@ int main()
                 printf("Account Number: %s\n", s_token); // First token (account number)
             }
             s_token = strtok(NULL, ","); // Get next token (mobile)
+            if (s_token != NULL)
+            {
+                strcpy(send_mobile, s_token); // First token (mobile number)
+            }
             s_token = strtok(NULL, ","); // Get next token (name)
             if (s_token != NULL)
             {
@@ -58,6 +67,7 @@ int main()
             break;     // Exit the loop after finding the record
         }
     }
+    fprintf(mess,"%s,",send_mobile);
     se_m = strtof(s_token, NULL);
 
     // If the account was not found, print a message
@@ -66,9 +76,13 @@ int main()
         printf(" Sender Account not registered!\n");
         fclose(file);
         fclose(temp);
+        fclose(mess);
+        remove(messfile);
+        remove(tempfile);
         exit(1);
     }
-    found = 0;
+    
+   
     printf("Enter Receiver Acc number:");
     scanf("%9s", receiver);
     // checking only receivers acc number
@@ -85,6 +99,10 @@ int main()
                 printf("Account Number: %s\t", r_token); // First token (account number)
             }
             r_token = strtok(NULL, ","); // Get next token (mobile)
+            if (r_token != NULL)
+            {
+                strcpy(rece_mobile, r_token); // First token (mobile number)
+            }
             r_token = strtok(NULL, ","); // Get next token (name)
             if (r_token != NULL)
             {
@@ -92,18 +110,21 @@ int main()
             }
 
             r_token = strtok(NULL, ","); // Get next token (balance)
-            found = 1;                   // Set the flag to 1 when found
+            round = 1;                   // Set the flag to 1 when found
             break;                       // Exit the loop after finding the record
         }
     }
     re_m = strtof(r_token, NULL);
 
     // If the account was not found, print a message
-    if (!found)
+    if (!round)
     {
         printf(" Receiver Account not registered!\n");
         fclose(file);
         fclose(temp);
+        fclose(mess);
+        remove(messfile);
+        remove(tempfile);
         exit(1);
     }
 
@@ -120,6 +141,12 @@ int main()
     re_m = re_m + balance;
     char send_m[20];
     char rec_m[20];
+    // output messages
+    // store sender sms
+    printf("\nHello User, Rs.%.2f has been debited from your \naccount number: %s. Cr.Bal: %.2f\n", balance, sender, se_m);
+    // store receiver sms
+    printf("\nHello User, Rs.%.2f has been credited to your \naccount number: %s. Cr.Bal: %.2f\n", balance, receiver, re_m);
+
     // Convert float to string
     sprintf(send_m, "%.2f", se_m);
     // Convert float to string
@@ -157,7 +184,6 @@ int main()
     fclose(file);
     fclose(temp);
 
-   
     if (remove(filename) != 0)
     {
         perror("Error deleting original file");
@@ -168,6 +194,25 @@ int main()
         perror("Error renaming temp file");
         exit(1);
     }
-    printf("Transaction completed successfully!\n");
+    printf("\nTransaction completed successfully!\n");
+   
+
+    // file for communication
+      // Write transaction details to mess file
+    int flag = fprintf(mess, "%s,%.2f,%s,%s,%.2f,%.2f\n", sender, se_m, rece_mobile, receiver, re_m, balance);
+    if (flag > 0)
+    {
+        printf("\nContents written successfully\n");
+    }
+    else
+    {
+        printf("Error Writing to File!\n");
+    }
+    fclose(mess);
+    int zero;
+    printf("0:");
+    scanf("%d",&zero);
+
+    remove(messfile);
     return 0;
 }
